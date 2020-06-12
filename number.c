37772733cd5620bdfcd31053d301782b3bce2e20
DivyaPathrabe <66524875+DivyaPathrabe@users.noreply.github.com>
2020-06-12 11:26:50 +0530

    created number.c
    With number other then 0 and 1. In case, you enter a number that is not in the list.

---
+++ number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+main()
+{
+printf("pick a number :\n2.1 \n3.2 \n4.3 \n5.4 \n6.5");
+int choice=0;
+scanf("%d",&choice);
+switch(choice)
+{
+case1:
+printf("You choose number 1");
+case2:
+printf("You choose number 2");
+case3:
+printf("You choose number 3");
+case4:
+printf("You choose number 4");
+case5:
+printf("You choose number 5");
+Break;
+default:
+printf("You enter a number is not in the list.");
+}
+}
